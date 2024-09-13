378243eb199314bba7b0150677115bcb243d83f4
HTKNgan007 <nganha2004nh@gmail.com>
2024-09-13 15:19:41 +0700

    Bai4_BMI

---
+++ Bai4_BMI/src/BMI/BMI.java
@@ -0,0 +1,28 @@
+package BMI;
+import java.util.Scanner;
+
+public class BMI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	float chieuCao, canNang, BMI ;
+		Scanner banPhim = new Scanner(System.in);
+		System.out.print("Nhap chieu cao(m): ");
+		//
+		chieuCao = banPhim.nextFloat();
+		System.out.print("Nhap can nang(kg): ");
+		//
+		canNang = banPhim.nextFloat();
+		BMI = canNang / (chieuCao * chieuCao);
+		if (BMI < 18.5) {
+	         System.out.println("Thieu can");
+	      } else if (BMI < 24.9) {
+			System.out.print("Binh thuong");
+		} else if (BMI < 29.9) {
+			System.out.print("Thua can");
+		} else if (BMI > 30) {
+			System.out.print("Beo Phi");
+		}
+	}
+
+}
