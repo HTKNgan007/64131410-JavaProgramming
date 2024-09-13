23c328aefb9b8c190ddbf41471ecd44e0f5598e3
HTKNgan007 <nganha2004nh@gmail.com>
2024-09-13 14:08:40 +0700

    Push BT2

--- Bai2_NhapXuat/src/NhapXuat/NhapXuat.java
+++ Bai2_NhapXuat/src/NhapXuat/NhapXuat.java
@@ -1,5 +1,24 @@
 package NhapXuat;
-
+import java.util.Scanner;
 public class NhapXuat {
-
+	public static void main(String[] args) {
+		//Khai báo các biến
+		int a, b, tong;
+		Scanner banPhim = new Scanner(System.in);
+		//Nhap du lieu
+		System.out.println("Tinh tong 2 so nguyen: ");
+		System.out.print("a=");
+		//Nhap du lieu cho bien a tu ban phim
+		a = banPhim.nextInt();
+		//Nhap du lieu cho b
+		System.out.print("b=");
+		//Nhap du lieu cho bien b tu ban phim
+		b = banPhim.nextInt();
+		//tong
+		tong = a + b;
+		//in tong ra 
+		System.out.print("tong = " + tong);
+		
+		
+	}
 }
