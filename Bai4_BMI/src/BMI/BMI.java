3b908cb4f6550e185802bdb3a619126347758113
HTKNgan007 <nganha2004nh@gmail.com>
2024-09-16 23:22:42 +0700

    Bai4_BMI them try_catch

--- Bai4_BMI/src/BMI/BMI.java
+++ Bai4_BMI/src/BMI/BMI.java
@@ -1,34 +1,58 @@
 package BMI;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BMI {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	float chieuCao, canNang, BMI ;
-	Scanner banPhim = new Scanner(System.in);
-		System.out.print("Nhap chieu cao(m): ");
-		chieuCao = banPhim.nextFloat();	
-	while (chieuCao > 2.5 || chieuCao < 0.3) {
-		System.out.print("Nhap lai chieu cao(m): ");
-		chieuCao = banPhim.nextFloat();
-	}	
-		System.out.print("Nhap can nang(kg): ");
-		canNang = banPhim.nextFloat();		
-	while (canNang > 200 || canNang < 1.3) {
-		System.out.print("Nhap lai can nang(kg): ");
-		canNang = banPhim.nextFloat();
-	}
-		BMI = canNang / (chieuCao * chieuCao);
-		if (BMI < 18.5) {
-	         System.out.println("Thieu can");
-	      } else if (BMI < 24.9) {
-			System.out.print("Binh thuong");
-		} else if (BMI < 29.9) {
-			System.out.print("Thua can");
-		} else if (BMI > 30) {
-			System.out.print("Beo Phi");
-		}
-	}
-
+    public static void main(String[] args) {
+        float chieuCao, canNang, BMI;
+        Scanner banPhim = new Scanner(System.in);
+        
+        //
+        while (true) {
+            try {
+                System.out.print("Nhập chiều cao(m): ");
+                chieuCao = banPhim.nextFloat();
+                
+                // Kiểm tra chiều cao hợp lệ
+                if (chieuCao > 2.5 || chieuCao < 0.3) {
+                    System.out.println("Chiều cao không hợp lệ, vui lòng nhập lại.");
+                } else {
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Nhập không đúng định dạng, vui lòng nhập lại.");
+                banPhim.next();
+            }
+        }
+        //
+        while (true) {
+            try {
+                System.out.print("Nhập cân nặng(kg): ");
+                canNang = banPhim.nextFloat();
+                
+                // Kiểm tra cân nặng hợp lệ
+                if (canNang > 200 || canNang < 1.3) {
+                    System.out.println("Cân nặng không hợp lệ, vui lòng nhập lại.");
+                } else {
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Nhập không đúng định dạng, vui lòng nhập lại.");
+                banPhim.next();
+            }
+        }
+        
+        // Tính toán BMI
+        BMI = canNang / (chieuCao * chieuCao);
+        if (BMI < 18.5) {
+            System.out.println("Thiếu cân");
+        } else if (BMI < 24.9) {
+            System.out.println("Bình thường");
+        } else if (BMI < 29.9) {
+            System.out.println("Thừa cân");
+        } else {
+            System.out.println("Béo phì");
+        }
+    }
 }
