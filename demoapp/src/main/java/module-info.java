f2c1f6fef8756d74cc38fd24868e4363051257b3
HTKNgan007 <nganha2004nh@gmail.com>
2024-10-06 14:10:54 +0700

    aaaaa

--- demoapp/src/main/java/module-info.java
+++ demoapp/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.demoapp {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
 
     opens com.example.demoapp to javafx.fxml;
     exports com.example.demoapp;
