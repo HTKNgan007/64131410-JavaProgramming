d2771a712c1f723af8b3d5feda3c1b61b06d09fe
HTKNgan007 <nganha2004nh@gmail.com>
2024-10-08 13:44:35 +0700

    aaaa

---
+++ BT_javafx_bmi/src/module-info.java
@@ -0,0 +1,5 @@
+module BT_javafx_bmi {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
--- demoapp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demoapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demoapp to javafx.fxml;
-    exports com.example.demoapp;
-}
